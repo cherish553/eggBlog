b94c4af21c358378fb787518bec5d90932bc699f
李可凡 <cherish@likefandeMacBook-Pro.local>
2019-11-12 15:45:53 +0800

    inner

--- config/config.default.js
+++ config/config.default.js
@@ -63,9 +63,7 @@ module.exports = appInfo => {
   };
   config.cluster = {
     listen: {
-      path: '',
       port: 3000,
-      hostname: '0.0.0.0',
     },
   };
   // add your user config here
